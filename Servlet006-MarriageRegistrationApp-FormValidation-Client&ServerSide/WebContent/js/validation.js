70f31a7fae669c381f39a54b2012deb64658fb2a
mac <mac@mac-PC>
2019-03-21 13:06:10 +0530

    first commit

---
+++ Servlet006-MarriageRegistrationApp-FormValidation-Client&ServerSide/WebContent/js/validation.js
@@ -0,0 +1,26 @@
+function validForm(form){
+	check.vflag.value="yes";
+	let name = check.name.value;
+	let age = check.age.value;
+	document.getElementById("errName").innerHTML="";
+	document.getElementById("errAge").innerHTML="";
+	if(name==""){
+		document.getElementById("errName").innerHTML="Please Enter Your Name";
+		return false;
+	}
+	else if(age==""){
+		document.getElementById("errAge").innerHTML="Please Enter Your Age";
+		return false;
+	}
+	else if(age<=0 || age>=125){
+		document.getElementById("errAge").innerHTML="Please Enter Age between 0 to 124";
+		return false;
+	}
+	else if(isNaN(age)){
+		document.getElementById("errAge").innerHTML="Please Enter Age as numeric value";
+		return false;
+	}
+	else{
+		return true;
+	}
+}
